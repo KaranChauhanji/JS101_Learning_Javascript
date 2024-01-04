81e02eb4a968377194f206fc9085aa40fd269cc8
KaranChauhanji <karanchauhanji2003@gmail.com>
2024-01-04 17:08:14 +0530

    adding problems of Day1

---
+++ index.js
@@ -0,0 +1,78 @@
+// Q1- Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+let school= "Masai School";
+let newline= "Transformation in Education";
+
+console.log(school);
+console.log(newline);
+
+// Q2-
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name="Karan";
+
+console.log(name);
+
+let fatherName = "Mr. Mukesh";
+
+console.log(fatherName);
+
+let motherName = "Mrs. Rajni";
+
+console.log(motherName);
+
+
+// Q3-Create a variable name to store your name
+// Create a variable age to store your age
+// Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+
+let fname = "Karan";
+let age = 20;
+
+console.log(typeof fname, fname);
+console.log(typeof age, age);
+
+// Q4-
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let lname = "Karan";
+let lschool= "Masai School";
+let grade = "Cap";
+let section = "07";
+let rollno = 1022;
+let HTML=90;
+let CSS=80;
+let Javascript=90;
+
+
+console.log(`  
+  ██████╗░███████╗░██████╗██╗░░░██╗██╗░░░░░████████╗
+  ██╔══██╗██╔════╝██╔════╝██║░░░██║██║░░░░░╚══██╔══╝
+  ██████╔╝█████╗░░╚█████╗░██║░░░██║██║░░░░░░░░██║░░░
+  ██╔══██╗██╔══╝░░░╚═══██╗██║░░░██║██║░░░░░░░░██║░░░
+  ██║░░██║███████╗██████╔╝╚██████╔╝███████╗░░░██║░░░
+  ╚═╝░░╚═╝╚══════╝╚═════╝░░╚═════╝░╚══════╝░░░╚═╝░░░`);
+console.log("******************************************************");
+
+console.log("Name:", lname);
+console.log("School:", lschool);
+console.log("Grade:", grade);
+console.log("Section:", section);
+console.log("Roll No:", rollno);
+
+console.log("*******************************************************");
+console.log(`
+                  𝕄𝕒𝕣𝕜𝕤 𝕆𝕓𝕥𝕒𝕚𝕟𝕖𝕕`);
+console.log("*******************************************************");
+console.log("HTML:", HTML);
+console.log("CSS:", CSS);
+console.log("Javascript:", Javascript);
