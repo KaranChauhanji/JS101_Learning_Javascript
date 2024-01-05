7e5c31b6ecbecadd8bcd046f5a630f9f934a8b53
KaranChauhanji <karanchauhanji2003@gmail.com>
2024-01-05 14:04:22 +0530

    day 2 problem

---
+++ index2.js
@@ -0,0 +1,48 @@
+//Enter code here
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+// problem2-
+function multiplyBy50(number) {
+    // Write code here
+    console.log(number*50);
+}
+
+// problem3-
+function productOfSix(one, two, three, four, five, six) {
+    // Write code here
+    console.log(one*two*three*four*five*six);
+}
+
+
+// problem4-
+
+function sumOfFive(one, two, three, four, five) {
+    // Write code here
+    console.log(one+two+three+four+five);
+}
+
+
+// problem5-
+function cubeOfNumber(N) {
+    // Write code here
+    console.log(N**3);
+}
+
+
+// problem6-
+function squareAndSum(one, two, three) {
+    // Write code here
+    let product= one**2+two**2+three**2;
+    console.log(product)
+  }
+
+  
+//   problem7-
+function findX(X) {
+    // Write code here
+    let product= X*3;
+    console.log(product+10)
+}
+
+
